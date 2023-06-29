8dd3469c629b722bea10e12081728ab45f99e56d
AndreDevFront <bitcoinberwian@gmail.com>
2023-06-29 07:32:33 -0300

    removed duplicated line.

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -25,7 +25,6 @@ import { moviesReducer } from './store/movies.reducer';
     StoreModule.forFeature('movies', moviesReducer),
     EffectsModule.forFeature([MoviesEffects]),
     RouterModule.forChild([{ path: '', component: SearchComponent }]),
-    StoreModule.forFeature('movies', moviesReducer),
     EffectsModule.forFeature([MoviesEffects]),
 
   ],
